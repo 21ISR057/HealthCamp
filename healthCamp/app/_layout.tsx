f8c3649306b1c73fbbc13bf79ba4e55cb813ab2f
Nivetha032 <nivisw04@gmail.com>
2025-02-26 10:24:31 +0530

    user profile,layot,gradle.build,home screen

--- healthCamp/app/_layout.tsx
+++ healthCamp/app/_layout.tsx
@@ -1,20 +1,14 @@
+import 'react-native-reanimated'; // ✅ Place at the topmost part
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from "./Screens/HomeScreen"; // ✅ Import HomeScreen
-
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
-
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -33,13 +27,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="RoleSelection" options={{ headerShown: false }} />
-        <Stack.Screen name="AdminNocUpload" options={{ title: "Upload NOC" }} />
+        <Stack.Screen name="NocUpload" options={{ title: "Upload NOC" }} />
         <Stack.Screen name="Home" options={{ title: "Home" }} />
-        <Stack.Screen name="CampDetails" options={{ title: "CampDetails" }} />
+        <Stack.Screen name="CampDetails" options={{ title: "Camp Details" }} />
+        <Stack.Screen name="userprofile" options={{ title: "User Profile" }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
